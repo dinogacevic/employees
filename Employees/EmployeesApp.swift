b9c1a29806d8a5c10065fede824cb0dd7082e37c
Dino Gačević <d.gacevic@nchain.com>
2024-10-15 18:33:21 +0200

    Initial Commit

---
+++ Employees/EmployeesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeesApp.swift
+//  Employees
+//
+//  Created by Dino Gačević on 15. 10. 24.
+//
+
+import SwiftUI
+
+@main
+struct EmployeesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
